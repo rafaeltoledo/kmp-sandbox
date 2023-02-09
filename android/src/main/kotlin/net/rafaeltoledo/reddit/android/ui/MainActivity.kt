cda310070b9ee17e2e783f1e2ebd34739a2660d5
Rafael Toledo <rafaeldtoledo@gmail.com>
2023-02-08 23:38:40 -0300

    Consume shared module on Android app

--- android/src/main/kotlin/net/rafaeltoledo/reddit/android/ui/MainActivity.kt
+++ android/src/main/kotlin/net/rafaeltoledo/reddit/android/ui/MainActivity.kt
@@ -1,5 +1,15 @@
 package net.rafaeltoledo.reddit.android.ui
 
+import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import net.rafaeltoledo.reddit.Platform
 
-class MainActivity : ComponentActivity()
+class MainActivity : ComponentActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    Log.d("RedditApp", Platform().name())
+  }
+}
